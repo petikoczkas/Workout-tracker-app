56546cb7d7a4006bea2bdadd86e892f646fb6c75
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-13 11:44:07 +0200

    Create base of the HomeScreen

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package hu.bme.aut.workout_tracker.ui.screen.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import hu.bme.aut.workout_tracker.R
+import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerDimens
+
+@Composable
+fun HomeScreen(
+    onSettingsClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = MaterialTheme.workoutTrackerDimens.gapNormal),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(onClick = { }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_logout),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = { onSettingsClick() }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_settings),
+                    contentDescription = null
+                )
+            }
+        }
+        Text("Home")
+    }
+}

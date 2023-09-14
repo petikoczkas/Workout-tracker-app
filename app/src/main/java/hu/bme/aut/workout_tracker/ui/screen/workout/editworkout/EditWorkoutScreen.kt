4e3d906fe895a44c5b1a264b1e24a614a7e031b8
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-14 20:07:49 +0200

    Update EditWorkoutScreen

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/editworkout/EditWorkoutScreen.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/editworkout/EditWorkoutScreen.kt
@@ -1,13 +1,14 @@
 package hu.bme.aut.workout_tracker.ui.screen.workout.editworkout
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerDimens
+import hu.bme.aut.workout_tracker.ui.view.button.AddButton
 
 @Composable
 fun EditWorkoutScreen() {
@@ -15,8 +16,12 @@ fun EditWorkoutScreen() {
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = workoutTrackerDimens.gapNormal),
-        verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text("Workout Name")
+        AddButton(
+            onClick = { /*TODO*/ },
+            text = "Add Exercise",
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }

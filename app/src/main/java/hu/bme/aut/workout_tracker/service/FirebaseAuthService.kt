99b937184da47183531a5f41b0781d561a7715e1
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-20 13:57:29 +0200

    Implement FirebaseAuthService

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/service/FirebaseAuthService.kt
@@ -0,0 +1,45 @@
+package hu.bme.aut.workout_tracker.service
+
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+object FirebaseAuthService {
+
+    suspend fun signIn(
+        firebaseAuth: FirebaseAuth,
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception?) -> Unit
+    ) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(it.exception)
+                }
+            }.await()
+    }
+
+    fun signOut(firebaseAuth: FirebaseAuth) {
+        firebaseAuth.signOut()
+    }
+
+    suspend fun registrate(
+        firebaseAuth: FirebaseAuth,
+        email: String,
+        password: String,
+        onSuccess: () -> Unit,
+        onFailure: (Exception?) -> Unit
+    ) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(it.exception)
+                }
+            }.await()
+    }
+}

d0a2673bdf49f146abde166745e667bb8a421d8f
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-20 14:01:35 +0200

    Create User model

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/data/model/User.kt
@@ -0,0 +1,8 @@
+package hu.bme.aut.workout_tracker.data.model
+
+data class User(
+    val id: String = "",
+    var name: String = "",
+    var photo: String = "",
+    val workouts: MutableList<String> = mutableListOf()
+)

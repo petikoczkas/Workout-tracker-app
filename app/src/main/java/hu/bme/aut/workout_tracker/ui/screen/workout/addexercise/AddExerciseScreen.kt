7d32d9756901752cc13bb0d9aeb01ea02b90aef1
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-20 18:07:07 +0200

    Change Bottom sheet to Dialog in AddExerciseScreen

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/addexercise/AddExerciseScreen.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/addexercise/AddExerciseScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -17,20 +15,20 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Dialog
 import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerDimens
 import hu.bme.aut.workout_tracker.ui.view.button.AddButton
 import hu.bme.aut.workout_tracker.ui.view.button.PrimaryButton
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddExerciseScreen(
 
 ) {
-    var showSheet by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
     var exercise by rememberSaveable { mutableStateOf("") }
 
-    if (showSheet) {
-        ModalBottomSheet(onDismissRequest = { showSheet = false }) {
+    if (showDialog) {
+        Dialog(onDismissRequest = { showDialog = false }) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -43,7 +41,7 @@ fun AddExerciseScreen(
                     label = { Text(text = "Name") },
                 )
                 PrimaryButton(
-                    onClick = { showSheet = false },
+                    onClick = { showDialog = false },
                     text = "Save",
                     modifier = Modifier
                         .fillMaxWidth()
@@ -60,7 +58,7 @@ fun AddExerciseScreen(
     ) {
         Text("Add Exercise")
         AddButton(
-            onClick = { showSheet = true },
+            onClick = { showDialog = true },
             text = "Create a custom exercise",
             modifier = Modifier
                 .fillMaxWidth()

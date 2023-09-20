38c3f7b5b51d714964045f9bc25d30e31e395676
Péter Koczkás <petikoczkas@hotmail.com>
2023-09-20 14:06:42 +0200

    Implement SettingsViewModel

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/settings/SettingsScreen.kt
@@ -1,5 +1,11 @@
 package hu.bme.aut.workout_tracker.ui.screen.settings
 
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,81 +20,139 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
 import hu.bme.aut.workout_tracker.R
+import hu.bme.aut.workout_tracker.ui.screen.settings.SettingsUiState.SettingsInit
+import hu.bme.aut.workout_tracker.ui.screen.settings.SettingsUiState.SettingsLoaded
+import hu.bme.aut.workout_tracker.ui.screen.settings.SettingsUiState.SettingsSaved
 import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerDimens
 import hu.bme.aut.workout_tracker.ui.view.button.PrimaryButton
 
 @Composable
 fun SettingsScreen(
-    onClick: () -> Unit
+    navigateBack: () -> Unit,
+    signOut: () -> Unit,
+    viewModel: SettingsViewModel = hiltViewModel()
 ) {
-    var name by rememberSaveable { mutableStateOf("Name of the User") }
+    val uiState by viewModel.uiState.collectAsState()
+    val updateUserFailedEvent by viewModel.updateUserFailedEvent.collectAsState()
 
+    when (uiState) {
+        SettingsInit -> {
+            viewModel.getCurrentUser()
+        }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = workoutTrackerDimens.gapNormal),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.End
-            ) {
-                IconButton(onClick = { }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_logout),
-                        contentDescription = null
-                    )
+        is SettingsLoaded -> {
+            val painter = rememberAsyncImagePainter(
+                if ((uiState as SettingsLoaded).imageUri == Uri.EMPTY) {
+                    viewModel.currentUser.photo
+                } else (uiState as SettingsLoaded).imageUri
+            )
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.GetContent()
+            ) { uri: Uri? ->
+                uri?.let {
+                    viewModel.onImageChange(it)
                 }
             }
-            Text(
-                text = "Settings",
-            )
-            Card(
-                shape = CircleShape,
+
+            Column(
                 modifier = Modifier
-                    .size(workoutTrackerDimens.settingsImageSize)
-                    .padding(workoutTrackerDimens.gapNormal)
+                    .fillMaxSize()
+                    .padding(horizontal = workoutTrackerDimens.gapNormal),
+                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_person),
-                    contentDescription = null,
+                Column(
                     modifier = Modifier
-                        .size(workoutTrackerDimens.settingsImageContentSize)
-                        .align(Alignment.CenterHorizontally),
-                )
-            }
-            TextField(
-                value = name,
-                onValueChange = { name = it },
-                label = { Text(text = "Name") },
-                modifier = Modifier
-                    .padding(
-                        vertical = workoutTrackerDimens.gapLarge,
+                        .fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        IconButton(
+                            onClick = {
+                                viewModel.signOut()
+                                signOut()
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_logout),
+                                contentDescription = null
+                            )
+                        }
+                    }
+                    Text(
+                        text = "Settings",
                     )
-            )
+                    Card(
+                        modifier = Modifier
+                            .size(workoutTrackerDimens.settingsImageSize)
+                            .padding(workoutTrackerDimens.gapNormal)
+                            .clip(CircleShape)
+                            .clickable { launcher.launch("image/*") }
+                    ) {
+                        if (viewModel.currentUser.photo.isEmpty() && (uiState as SettingsLoaded).imageUri == Uri.EMPTY) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_person),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(workoutTrackerDimens.settingsImageContentSize)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                        } else {
+                            Image(
+                                painter = painter,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(workoutTrackerDimens.settingsImageSize)
+                                    .align(Alignment.CenterHorizontally)
+                            )
+                        }
+                    }
+                    TextField(
+                        value = (uiState as SettingsLoaded).name,
+                        onValueChange = viewModel::onNameChange,
+                        label = { Text(text = "Name") },
+                        modifier = Modifier
+                            .padding(
+                                vertical = workoutTrackerDimens.gapLarge,
+                            )
+                    )
+                }
+                PrimaryButton(
+                    onClick = { viewModel.saveButtonOnClick() },
+                    text = "Save",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = workoutTrackerDimens.gapNormal)
+                )
+                if (updateUserFailedEvent.isUpdateUserFailed) {
+                    Toast.makeText(
+                        LocalContext.current,
+                        updateUserFailedEvent.exception?.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    viewModel.handledUpdateUserFailedEvent()
+                }
+            }
         }
-        PrimaryButton(
-            onClick = { onClick() },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = workoutTrackerDimens.gapNormal),
-            text = "Save"
-        )
+
+        SettingsSaved -> {
+            navigateBack()
+        }
     }
 }

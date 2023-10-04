f8b17127d4ebc4fdad6c4ad438723b52c5cb6048
Péter Koczkás <petikoczkas@hotmail.com>
2023-10-04 14:10:40 +0200

    Create WorkoutUiState

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/workout/WorkoutUiState.kt
@@ -0,0 +1,13 @@
+package hu.bme.aut.workout_tracker.ui.screen.workout.workout
+
+sealed class WorkoutUiState {
+
+    data object WorkoutInit : WorkoutUiState()
+    data class WorkoutLoaded(
+        val weightList: List<List<String>>,
+        val repsList: List<List<String>>,
+        val isEnabledList: List<Boolean>
+    ) : WorkoutUiState()
+
+    data object WorkoutSuccess : WorkoutUiState()
+}

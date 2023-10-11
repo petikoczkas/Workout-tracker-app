ec20e00031b261d4bd805a0d9071deb55da609f8
Péter Koczkás <petikoczkas@hotmail.com>
2023-10-11 18:23:14 +0200

    Create StandingUiState

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/standings/StandingUiState.kt
@@ -0,0 +1,11 @@
+package hu.bme.aut.workout_tracker.ui.screen.standings
+
+import hu.bme.aut.workout_tracker.data.model.Exercise
+
+sealed class StandingUiState {
+    data object StandingInit : StandingUiState()
+
+    data class StandingLoaded(
+        val selectedItem: Exercise,
+    ) : StandingUiState()
+}

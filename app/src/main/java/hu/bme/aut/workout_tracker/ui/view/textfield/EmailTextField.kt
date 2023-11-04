5b5b7e4c6768ca75ad7aca8374688227fd963516
Péter Koczkás <petikoczkas@hotmail.com>
2023-11-04 18:19:34 +0100

    Create EmailTextField

---
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/view/textfield/EmailTextField.kt
@@ -0,0 +1,29 @@
+package hu.bme.aut.workout_tracker.ui.view.textfield
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import hu.bme.aut.workout_tracker.R
+import hu.bme.aut.workout_tracker.utils.isValidEmail
+
+@Composable
+fun EmailTextField(
+    email: String,
+    onEmailChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    WorkoutTrackerTextField(
+        text = email,
+        onTextChange = onEmailChange,
+        label = stringResource(R.string.email),
+        isError = if (email.isNotEmpty()) !email.isValidEmail() else false,
+        leadingIcon = {
+            Icon(painter = painterResource(id = R.drawable.ic_email), contentDescription = null)
+        },
+        keyBoardType = KeyboardType.Email,
+        modifier = modifier
+    )
+}

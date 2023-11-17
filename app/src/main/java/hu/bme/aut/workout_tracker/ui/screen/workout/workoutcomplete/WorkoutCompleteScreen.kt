069c0822671c55d8a78332464cb6a6de1c571c65
Péter Koczkás <petikoczkas@hotmail.com>
2023-11-17 20:45:54 +0100

    Style WorkoutCompleteScreen

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/workoutcomplete/WorkoutCompleteScreen.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/workout/workoutcomplete/WorkoutCompleteScreen.kt
@@ -8,32 +8,46 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.res.stringResource
 import hu.bme.aut.workout_tracker.R
 import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerDimens
+import hu.bme.aut.workout_tracker.ui.theme.workoutTrackerTypography
 import hu.bme.aut.workout_tracker.ui.view.button.PrimaryButton
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun WorkoutCompleteScreen(
+    focusManager: FocusManager,
     navigateToAddExercise: () -> Unit,
-    navigateBack: () -> Unit
+    navigateBack: () -> Unit,
 ) {
-    val keyboardController = LocalSoftwareKeyboardController.current
-    keyboardController?.hide()
+    focusManager.clearFocus()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = workoutTrackerDimens.gapNormal),
+            .padding(horizontal = workoutTrackerDimens.gapLarge),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(stringResource(R.string.workout_complete))
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .weight(1f),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = stringResource(R.string.workout_complete),
+                style = workoutTrackerTypography.titleTextStyle,
+                modifier = Modifier
+                    .padding(top = workoutTrackerDimens.gapVeryLarge)
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
             verticalArrangement = Arrangement.Center
         ) {
             PrimaryButton(
@@ -41,7 +55,7 @@ fun WorkoutCompleteScreen(
                 text = stringResource(R.string.add_exercise),
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = workoutTrackerDimens.gapNormal)
+                    .padding(bottom = workoutTrackerDimens.gapLarge)
             )
             PrimaryButton(
                 onClick = navigateBack,
@@ -49,5 +63,8 @@ fun WorkoutCompleteScreen(
                 modifier = Modifier.fillMaxWidth()
             )
         }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .weight(1f)) {}
     }
 }

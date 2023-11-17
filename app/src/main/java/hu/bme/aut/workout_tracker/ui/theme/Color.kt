b464b8afcb6ecea5eac9ac7fc495ee45aaa65510
Péter Koczkás <petikoczkas@hotmail.com>
2023-11-17 20:34:06 +0100

    add base colors

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/theme/Color.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/theme/Color.kt
@@ -3,29 +3,45 @@ package hu.bme.aut.workout_tracker.ui.theme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 data class WorkoutTrackerColors(
 
-    private val purple80: Color = Color(0xFFD0BCFF),
-    private val purpleGrey80: Color = Color(0xFFCCC2DC),
-    private val pink80: Color = Color(0xFFEFB8C8),
+    val darkPrimary: Color = Color(0xFF5089BC),
+    val darkBackground: Color = Color(0xFF0F1213),
+    val darkSurfaceVariant: Color = Color(0xFF373840),
+    val darkOnSurfaceVariant: Color = Color(0xFF9EA2AE),
 
-    private val purple40: Color = Color(0xFF6650a4),
-    private val purpleGrey40: Color = Color(0xFF625b71),
-    private val pink40: Color = Color(0xFF7D5260),
+
+    val tertiary: Color = Color(0xFF0B7F00),
+    val error: Color = Color(0xFFCE0328),
+
+    val gold: Color = Color(0xFFFFD700),
+    val silver: Color = Color(0xFFC0C0C0),
+    val bronze: Color = Color(0xFFCD7F32),
 
     val darkColorScheme: ColorScheme = darkColorScheme(
-        primary = purple80,
-        secondary = purpleGrey80,
-        tertiary = pink80
+        primary = darkPrimary,
+        onPrimary = darkBackground,
+        background = darkBackground,
+        surface = darkBackground,
+        surfaceVariant = darkSurfaceVariant,
+        onSurfaceVariant = darkOnSurfaceVariant,
+        error = error,
+        onError = darkBackground,
+        tertiary = tertiary
+
     ),
 
     val lightColorScheme: ColorScheme = lightColorScheme(
-        primary = purple40,
-        secondary = purpleGrey40,
-        tertiary = pink40
+        /*primary = purple40,
+         secondary = purpleGrey40,
+         tertiary = pink40*/
+        error = error,
+        tertiary = tertiary
 
         /* Other default colors to override
         background = Color(0xFFFFFBFE),
@@ -40,3 +56,8 @@ data class WorkoutTrackerColors(
 )
 
 val LocalColors = staticCompositionLocalOf { WorkoutTrackerColors() }
+
+val workoutTrackerColors: WorkoutTrackerColors
+    @Composable
+    @ReadOnlyComposable
+    get() = staticCompositionLocalOf { WorkoutTrackerColors() }.current

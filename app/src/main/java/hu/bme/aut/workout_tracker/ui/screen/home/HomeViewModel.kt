2f94f1971172e30a1b735e7fc58d8d1c554bdfb2
Péter Koczkás <petikoczkas@hotmail.com>
2023-11-17 20:41:52 +0100

    Rework HomeScreen

--- app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/hu/bme/aut/workout_tracker/ui/screen/home/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.bme.aut.workout_tracker.data.model.Exercise
 import hu.bme.aut.workout_tracker.data.model.User
 import hu.bme.aut.workout_tracker.data.model.Workout
 import hu.bme.aut.workout_tracker.ui.WorkoutTrackerPresenter
@@ -26,13 +27,21 @@ class HomeViewModel @Inject constructor(
     private val _uiState = MutableStateFlow<HomeUiState>(HomeInit)
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
-    var currentUser = User()
+    private var currentUser = User()
 
     private val _workouts = MutableLiveData<List<Workout>>()
     val workouts: LiveData<List<Workout>> = _workouts
 
+    private val _exercises = MutableLiveData<List<Exercise>>()
+    val exercises: LiveData<List<Exercise>> = _exercises
+
+    fun getUserLastName(): String {
+        return currentUser.name.substringAfter(" ")
+    }
+
     fun getFavoriteWorkouts() {
         _uiState.value = HomeLoaded
+        getStandingsExercises()
         viewModelScope.launch {
             currentUser = workoutTrackerPresenter.getCurrentUser()
             withContext(Dispatchers.IO) {
@@ -41,5 +50,30 @@ class HomeViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    private fun getStandingsExercises() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                workoutTrackerPresenter.getStandingsExercises().collect {
+                    _exercises.postValue(it)
+                }
+            }
+        }
+    }
+
+    fun getMaxList(exercises: List<Exercise>): List<List<String>> {
+        val maxList = mutableListOf<List<String>>()
+        for (e in exercises) {
+            val tmp = mutableListOf<String>()
+            tmp.add(e.name.substringAfter(" "))
+            if (currentUser.oneRepMaxCharts.containsKey(e.id)) {
+                tmp.add(currentUser.oneRepMaxCharts[e.id]?.max()?.toInt().toString())
+            } else {
+                tmp.add("-")
+            }
+            maxList.add(tmp.toList())
+        }
+        return maxList.toList()
     }
 }

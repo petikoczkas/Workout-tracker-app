1f6e3d99c0940ee1efe6560a2c88b1b90e84e2cc
Péter Koczkás <petikoczkas@hotmail.com>
2023-11-19 20:30:34 +0100

    Add lottie dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
 
     implementation("com.patrykandpatrick.vico:compose-m3:1.12.0")
 
+    implementation("com.airbnb.android:lottie-compose:6.2.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

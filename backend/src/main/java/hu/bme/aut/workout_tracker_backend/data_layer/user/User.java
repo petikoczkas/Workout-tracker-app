020ff5165f1ea68650a5a88fe5b6477d5a66db6f
Koczkás Péter <petikoczkas@hotmail.com>
2024-02-20 17:31:12 +0100

    Create repositories

--- backend/src/main/java/hu/bme/aut/workout_tracker_backend/data_layer/user/User.java
+++ backend/src/main/java/hu/bme/aut/workout_tracker_backend/data_layer/user/User.java
@@ -1,6 +1,5 @@
 package hu.bme.aut.workout_tracker_backend.data_layer.user;
 
-import hu.bme.aut.workout_tracker_backend.data_layer.exercise.Exercise;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -9,9 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Entity
 @Data
@@ -21,6 +18,7 @@ public class User {
 
     @Id
     private String email;
+    private String password;
     private String name;
     private String photo;
     @ElementCollection
---
+++ backend/src/main/java/hu/bme/aut/workout_tracker_backend/data_layer/workout/WorkoutRepository.java
@@ -0,0 +1,15 @@
+package hu.bme.aut.workout_tracker_backend.data_layer.workout;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WorkoutRepository extends CrudRepository<Workout, Long> {
+
+    @Query("SELECT w FROM Workout w WHERE w.userId = :id")
+    Optional<List<Workout>> findByUserId(String id);
+}

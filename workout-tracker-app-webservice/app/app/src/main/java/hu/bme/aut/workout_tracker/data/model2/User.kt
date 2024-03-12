957a8bb3e8fccbaecf7b984be0b0098a4bffc88c
Peter Koczkas <petikoczkas@hotmail.com>
2024-03-12 12:37:33 +0100

    Add models

---
+++ workout-tracker-app-webservice/app/app/src/main/java/hu/bme/aut/workout_tracker/data/model2/User.kt
@@ -0,0 +1,17 @@
+package hu.bme.aut.workout_tracker.data.model2
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class User(
+    @Json(name = "email")
+    val email: String,
+    @Json(name = "firstName")
+    val firstName: String,
+    @Json(name = "lastName")
+    val lastName: String,
+    @Json(name = "photo")
+    val photo: String
+)

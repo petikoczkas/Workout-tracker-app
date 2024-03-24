32311bfb7ee50b638c1a0311f1978d7b73706cdd
Peter Koczkas <peter.koczkas@applion.hu>
2024-03-24 16:31:00 +0100

    Delete old models

--- workout-tracker-app-webservice/app/app/src/main/java/hu/bme/aut/workout_tracker/data/model_DEPRICATED/Exercise.kt
+++
@@ -1,9 +0,0 @@
-package hu.bme.aut.workout_tracker.data.model_DEPRICATED
-
-import java.util.UUID
-
-data class Exercise(
-    val id: String = UUID.randomUUID().toString(),
-    val category: String = "",
-    val name: String = ""
-)
